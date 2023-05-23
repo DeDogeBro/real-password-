b3645a0c31e8e4981fafe04813a913b6f193db06
jasonjanuary2011 <92645207+jasonjanuary2011@users.noreply.github.com>
2023-05-22 17:45:38 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var password = "elvinistiny1!andbird^";
+function passcheck(){
+
+if(document.getElementById('pass1').value != password){
+alert('wrong password try again :)');    
+return false;
+}  
+
+if(document.getElementById('pass1').value == password){
+    alert('you got it :D go to https://sites.google.com/7hueh-6rgbver-dviugyie-gfiew1 and dont tell anybody');    
+    }    
+}
